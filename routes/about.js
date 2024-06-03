c3d0859cf8b2f79f98f33fbe082ed63d540981ab
Ravi-dhanani <117377500+Ravi-dhanani@users.noreply.github.com>
2024-06-03 15:18:35 +0530

    authentication add in api

--- routes/about.js
+++ routes/about.js
@@ -2,11 +2,12 @@ var express = require("express");
 var router = express.Router();
 
 var about = require("../controller/about");
+var auth = require("../middleware/auth");
 
-router.post("/add_about", about.add_about);
+router.post("/add_about", auth.check_token, about.add_about);
 router.get("/get_about", about.get_about);
-router.get("/delete_about/:id", about.delete_about);
+router.get("/delete_about/:id", auth.check_token, about.delete_about);
 router.get("/get_about_data/:id", about.get_about_data);
-router.put("/update_about/:id", about.update_about);
+router.put("/update_about/:id", auth.check_token, about.update_about);
 
 module.exports = router;
--- routes/language.js
+++ routes/language.js
@@ -2,10 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 var language = require("../controller/language");
+var auth = require("../middleware/auth");
 
-router.post("/add_language", language.add_Language);
+router.post("/add_language", auth.check_token, language.add_Language);
 router.get("/get_language", language.get_Languages);
-router.put("/update_language/:id", language.update_language);
-router.get("/delete_language/:id", language.delete_language);
+router.put("/update_language/:id", auth.check_token, language.update_language);
+router.get("/delete_language/:id", auth.check_token, language.delete_language);
 
 module.exports = router;
