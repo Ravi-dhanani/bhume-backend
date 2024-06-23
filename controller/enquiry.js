644993dc6226f968989503b70ae2cd75c75a6ae6
Ravi-dhanani <ravidhanani78@gmail.com>
2024-06-23 23:00:56 +0530

    date add in enquiry

--- controller/enquiry.js
+++ controller/enquiry.js
@@ -4,7 +4,7 @@ var enquiry = require("../model/enquiry");
 
 exports.add_enquiry = async (req, res) => {
   try {
-    req.body.Date = new Date().format("DD/MM/YYYY");
+    req.body.Date = new Date().toISOString().slice(0, 10);
     var data = await enquiry.create(req.body);
     res.status(200).json({
       message: "Enquiry Add Successfully",
