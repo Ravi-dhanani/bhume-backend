74637d9f363edd758216cb7f330df35fdf89f0b4
Ravi-dhanani <ravidhanani78@gmail.com>
2024-06-23 22:48:11 +0530

    enquiry spelling right

--- routes/enquiry.js
+++ routes/enquiry.js
@@ -6,6 +6,6 @@ var enquiry = require("../controller/enquiry");
 router.post("/add_enquiry", enquiry.add_enquiry);
 router.get("/get_enquiry", enquiry.get_enquiry);
 router.put("/update_enquiry/:id", enquiry.update_enquiry);
-router.get("/delete_enquiry/:id", enquiry.update_enquiry);
+router.get("/delete_enquiry/:id", enquiry.delete_Enquiry);
 
 module.exports = router;
