f15a326bb3630fed76480354eefae2dacbc906b3
Ravi-dhanani <117377500+Ravi-dhanani@users.noreply.github.com>
2024-06-28 00:03:06 +0530

    Update career.js

--- routes/career.js
+++ routes/career.js
@@ -6,6 +6,7 @@ var career = require("../controller/carrer");
 router.post("/add_career", career.add_career);
 router.get("/get_career", career.get_career);
 router.get("/get_single_career/:id", career.get_single_career);
+router.get("/get_Career_data/:id", about.get_Career_data);
 router.get("/delete_career/:id", career.delete_career);
 router.put("/update_career/:id", career.update_career);
 
